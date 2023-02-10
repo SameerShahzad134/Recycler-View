3b75beedb153b26acc2da6c6274d18dcddf29a39
Sameer Shahzad <bcsf19a030@pucit.edu.pk>
2023-02-10 17:10:13 +0500

    Recycler View Starts

---
+++ app/src/main/java/com/example/recylcerview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.recylcerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity<MyAdapter> extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private List<String> itemList;
+    private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        itemList = new ArrayList<>();
+        itemList.add("Item 1");
+        itemList.add("Item 2");
+        itemList.add("Item 3");
+        itemList.add("Item 4");
+        itemList.add("Item 5");
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new MyAdapter(this, itemList);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter((RecyclerView.Adapter) adapter);
+}
